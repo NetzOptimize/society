94a62827abb12b7a1df72e8f5592f0189eb8cd97
Savita <savita.netzoptimize@gmail.com>
2023-04-04 14:01:12 +0530

    Update detail.blade.php

--- resources/views/houses/detail.blade.php
+++ resources/views/houses/detail.blade.php
@@ -1,17 +1,20 @@
 <h3>Details of House {{ $house->full_address }}</h3>
 
 <h2>Owner Of The House</h2>
-Name:{{ $owner->name }}
-Mobile 1: {{ $owner->mobile1 }}
-Mobile 2: {{ $owner->mobile2 }}
-
+@if ($owner)
+    Name:{{ $owner->name }}
+    Mobile 1: {{ $owner->mobile1 }}
+    Mobile 2: {{ $owner->mobile2 }}
+@endif
 <h2>Tenants Of The House</h2>
-@foreach ($tenants as $tenant)
-    Name:{{ $tenant['name'] }}
-    Mobile 1: {{ $tenant['mobile1'] }}
-    Mobile 2: {{ $tenant['mobile2'] }}
-@endforeach
 
+@if ($tenants)
+    @foreach ($tenants as $tenant)
+        Name:{{ $tenant['name'] }}
+        Mobile 1: {{ $tenant['mobile1'] }}
+        Mobile 2: {{ $tenant['mobile2'] }}
+    @endforeach
+@endif
 {{-- payment listing --}}
 <table>
     <tr>
@@ -32,21 +35,23 @@ Mobile 2: {{ $owner->mobile2 }}
             MODE OF PAYMENT
         </th>
     </tr>
-    <tr>
-        @php
-        $i= 0;
-    @endphp
-        @foreach ($payments as $payment)
+    @if ($payments->first())
+        <tr>
             @php
-                $i++;
+                $i = 0;
             @endphp
-            <td>@php echo $i; @endphp</td>
-            <td>{{ $payment->billingmonth }}</td>
+            @foreach ($payments as $payment)
+                @php
+                    $i++;
+                @endphp
+                <td>@php echo $i; @endphp</td>
+                <td>{{ $payment->billingmonth }}</td>
 
-            <td>{{ $payment->amount }}</td>
-            <td>{{ $payment->dateofdeposit }}</td>
-            <td>{{ $payment->paymentmode->name }}</td>
-    </tr>
+                <td>{{ $payment->amount }}</td>
+                <td>{{ $payment->dateofdeposit }}</td>
+                <td>{{ $payment->paymentmode->name }}</td>
+        </tr>
     @endforeach
+    @endif
 
 </table>
