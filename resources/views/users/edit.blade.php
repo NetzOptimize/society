763e97ba08c29060c081b5e8c7f5f5d4bdf26311
Shivamsharma9464212234 <125370902+Shivamsharma9464212234@users.noreply.github.com>
2023-04-06 18:48:12 +0530

    changes form
    dsafsdf

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -34,7 +34,7 @@
         </div>
 
         <label>USER-TYPE</label>
-        <select name="usertype_id" class="form-control">
+        <select name="usertype_id" class="form-control user-type1">
             @foreach ($usertypes as $usertype)
                 @if ($user->usertype_id == $usertype->id)
                     <option value="{{ $usertype->id }}" selected>{{ $usertype->role }}</option>
