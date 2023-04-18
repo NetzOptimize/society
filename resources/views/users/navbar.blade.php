23e56423abf8fa977ce4a06a2aca3c39e5e3b46f
Shivamsharma9464212234 <125370902+Shivamsharma9464212234@users.noreply.github.com>
2023-04-18 12:44:40 +0530

    user-nav-change

--- resources/views/users/navbar.blade.php
+++ resources/views/users/navbar.blade.php
@@ -7,11 +7,20 @@
 <div class="user-logo">
         <img src="{{asset('logo.png')}}" class="rounded-pill" style="height: 60px;
     width: 70px;" alt="">
+    
       </div>
+   
 <!-- <h4>WELCOME {{ Auth::user()->name }}</h4> -->
 
-<div class="user dropdown pe-3 me-3">
+<div class="user dropdown pe-3 me-3 d-flex align-items-center">
+   <div class="user-links  p-3">
+<nav class="navbar d-flex justify-content-end" id="users-nav">
 
+    <a href="{{ route('user.profile.edit') }}"  class="btn btn-light me-3 border" >My Account</a>
+    <a href="{{ route('user.report') }}"  class="btn btn-light border">Reports</a>
+
+</nav>
+</div>
 <a class="btn bg-transparent dropdown-toggle  text-light" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
  <img src="{{asset('user1.jpg')}}" alt="" class="rounded-pill me-2" height="30px" width="30px" >User </a>
 <div class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton">
@@ -22,13 +31,6 @@
 </div>
 </div>
 
-<div class="user-links  bg-light mt-3 p-3">
-<nav class="navbar d-flex justify-content-end">
 
-    <a href="{{ route('user.profile.edit') }}"  class="btn btn-light me-3 border" >My Account</a>
-    <a href="{{ route('user.report') }}"  class="btn btn-light border">Reports</a>
-
-</nav>
-</div>
 </div>
 
